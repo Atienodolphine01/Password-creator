95c2cd843ba43a98dbe460c4b5cc6e526dce4ede
Dolphine Atieno <awuordolphine65@gmail.com>
2020-04-20 17:13:59 +0300

    created users objects that he/she will input

---
+++ user_details.py
@@ -0,0 +1,9 @@
+class user:
+    '''
+    Class that generates instance of user details
+    '''
+#Test one
+
+    users_list=[] #users will be saved here.
+
+    def _init_(self, first_name, last_name, password):
