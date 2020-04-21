7c77f2fb15baf3f94c5be2da5e17e070ae8a30b8
Dolphine Atieno <awuordolphine65@gmail.com>
2020-04-21 13:54:41 +0300

    commented everything below elif short_code line 101 to try and correct the indention error

--- run.py
+++ run.py
@@ -128,76 +128,84 @@ def main():
                             print('\n')
                             print("-"*60)
                             print("Choose the password option you would like. Use keys: \n ep - To enter a password \n gp - To generate a password \n ex - Exit")
-                            password_choice = input("Enter an option: ").lower().strip()
-                            print("-"*60)
-                            if password_choice == 'ep':
-                                print('\n')
-                                password = input("Enter your password: ").strip()
-                                break
-                            elif password_choice == 'gp':
-                                password = generate_password(password)
-                                break
-                            elif password_choice == 'ex':
-                                break
-                            else:
-                                print("You entered the wrong option please try again.")
-                        save_detail(create_detail(user_name, website_name, account_name, password))
-                        print('\n')
-                        print(f"Details created: \n Website name: {website_name} - Account name: {account_name} - Password: {password}")
-
-                    elif short_code == 'dc':
-                        print('\n')
-
-                        if display_details(user_name):
-                            print("Here is a list of all your credentials:")
-                            print('\n')
-                            for detail in display_details(user_name):
-                                print(f"Website name: {detail.website_name} - Account name: {detail.account_name} - Password: {detail.password}")
-                        else:
-                            print('\n')
-                            print("You don't seem to have any credential saved yet.")
-                            print('\n')
-
-
-                    elif short_code == 'fc':
-                        search_website = input("Enter the website name you are searching for: \n")
-                        if check_existing_details(search_website):
-                            result = find_my_website(search_website)
-                            print(f"Search result: Website name: {result.website_name} - Account name: {result.account_name} - Password: {result.password}")
-                        else:
-                            print("No such credential exists. Please try again!")
                     
-                    elif short_code == 'cp':
-                        print('\n')
-                        chosen_website = input("Enter the website name for the details you want to copy: ")
-                        copy_detail(chosen_website)
-                        print('\n')
-                    elif short_code == 'del':
-                        print('\n')
-                        print("Enter the name of the website credential you want to delete.")
-                        print('\n')
-                        answer = input("Enter choice: ")
+                    
 
-                        if check_existing_details(answer):
-                            answer = find_my_website(answer)
-                            delete_detail(answer)
-                            print("Credentials successfully deleted!")
 
-                        else:
-                            ("No such detail exists!")
+#                         while True:
+#                             print('\n')
+#                             print("-"*60)
+#                             print("Choose the password option you would like. Use keys: \n ep - To enter a password \n gp - To generate a password \n ex - Exit")
+#                             password_choice = input("Enter an option: ").lower().strip()
+#                             print("-"*60)
+#                             if password_choice == 'ep':
+#                                 print('\n')
+#                                 password = input("Enter your password: ").strip()
+#                                 break
+#                             elif password_choice == 'gp':
+#                                 password = generate_password(password)
+#                                 break
+#                             elif password_choice == 'ex':
+#                                 break
+#                             else:
+#                                 print("You entered the wrong option please try again.")
+#                         save_detail(create_detail(user_name, website_name, account_name, password))
+#                         print('\n')
+#                         print(f"Details created: \n Website name: {website_name} - Account name: {account_name} - Password: {password}")
+
+#                     elif short_code == 'dc':
+#                         print('\n')
+
+#                         if display_details(user_name):
+#                             print("Here is a list of all your credentials:")
+#                             print('\n')
+#                             for detail in display_details(user_name):
+#                                 print(f"Website name: {detail.website_name} - Account name: {detail.account_name} - Password: {detail.password}")
+#                         else:
+#                             print('\n')
+#                             print("You don't seem to have any credential saved yet.")
+#                             print('\n')
+
+
+#                     elif short_code == 'fc':
+#                         search_website = input("Enter the website name you are searching for: \n")
+#                         if check_existing_details(search_website):
+#                             result = find_my_website(search_website)
+#                             print(f"Search result: Website name: {result.website_name} - Account name: {result.account_name} - Password: {result.password}")
+#                         else:
+#                             print("No such credential exists. Please try again!")
+                    
+#                     elif short_code == 'cp':
+#                         print('\n')
+#                         chosen_website = input("Enter the website name for the details you want to copy: ")
+#                         copy_detail(chosen_website)
+#                         print('\n')
+#                     elif short_code == 'del':
+#                         print('\n')
+#                         print("Enter the name of the website credential you want to delete.")
+#                         print('\n')
+#                         answer = input("Enter choice: ")
+
+#                         if check_existing_details(answer):
+#                             answer = find_my_website(answer)
+#                             delete_detail(answer)
+#                             print("Credentials successfully deleted!")
+
+#                         else:
+#                             ("No such detail exists!")
                         
-                    else:
-                        print("You entered the wrong option. Please try again.")
+#                     else:
+#                         print("You entered the wrong option. Please try again.")
                 
-                else:
-                    print('\n')
-                    print("You entered the wrong details Please try again or create an account.")
+#                 else:
+#                     print('\n')
+#                     print("You entered the wrong details Please try again or create an account.")
 
-            else:
-                print("-"*60)
-                print('\n')
-                print("I really didn't get that. Please use the short codes.")
+#             else:
+#                 print("-"*60)
+#                 print('\n')
+#                 print("I really didn't get that. Please use the short codes.")
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
